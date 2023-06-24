29fd96740d871f5c754f64683a3ff010481722f2
Abhishek <abhisahu1908@gmail.com>
2023-06-24 15:25:41 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./App.css";
 // import Image from "./Assets/Image.jpg";
 function App() {
   return (
-  
+    <React.Fragment >
+      <div className="flex">
     <Weather />
-  
+    </div>
+    </React.Fragment>
   );
 }
 
